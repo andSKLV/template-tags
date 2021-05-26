04cb210bfa5a027d8fddea75aa11c53d46b39700
andrey.sokolov <andrey.sokolov@nordigy.ru>
2021-05-26 16:08:22 +0300

    Add qs

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const items = [
 	"Пройти туториал по Реакту",
 	"Сделать фронт для своего проекта",
 ];
+
+const itemTemplate = document.querySelector(".item_template").content;
+const list = document.querySelector(".list");
+const formButton = document.querySelector(".form__submit");
+const formInput = document.querySelector(".form__input");
