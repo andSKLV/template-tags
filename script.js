f384403fbed5c0c56296bf4c037593bb7171ab95
andrey.sokolov <andrey.sokolov@nordigy.ru>
2021-05-26 19:41:36 +0300

    feat: render

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ const itemTemplate = document.querySelector(".item_template").content;
 const list = document.querySelector(".list");
 const formButton = document.querySelector(".form__submit");
 const formInput = document.querySelector(".form__input");
+
+function renderItems() {
+	items.forEach(renderItem);
+}
+
+function renderItem(text) {
+	const htmlElement = itemTemplate.cloneNode(true);
+	htmlElement.querySelector('.item__text').innerText = text;
+	list.appendChild(htmlElement);
+}
+
+function handleSubmit() {
+	renderItem(formInput.value);
+}
+
+formButton.addEventListener('click', handleSubmit);
+
+renderItems();
